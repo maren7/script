bdf64f7d05984dc4a2ad8daa72c64cb9406340be
Maren <31197527+maren7@users.noreply.github.com>
2021-05-31 18:01:52 -0300

    Update maren-script.sh

--- maren-script.sh
+++ maren-script.sh
@@ -1,9 +1,7 @@
 #!/bin/bash
 echo CTRL+C para parar
-read -p "CÓDIGO AUTOMATIZADO INSTALAÇÃO DO AMBIENTE DE DESENVOLVIMENTO DO BITCOIN" -t 2
-read -p "BAIXAR CÓDIGO FONTE DA REALBIT E COMPILAR AUTOMATICAMENTE" -t 3
 read -p "Instalando pacotes -- maren..." -t 1
-sudo apt-get install git python-pip -y && sudo pip install scrypt construct==2.5.2 -y && pip3 install base58 ecdsa -y && sudo apt-get install git build-essential libtool autotools-dev automake autoconf pkg-config libboost- all-dev libevent-dev gdb libssl-dev -y && sudo apt install build-essential libtool autotools-dev automake pkg-config bsdmainutils curl git -y && sudo apt install nsis && sudo apt install g++-mingw-w64-x86-64 && sudo apt-get install build-essential libtool autotools-dev automake pkg-config bsdmainutils && sudo apt-get install libevent-dev libboost-dev libboost-system-dev libboost-filesystem-dev libboost-test-dev && sudo apt install libsqlite3-dev && sudo apt install libminiupnpc-dev libnatpmp-dev && sudo apt-get install libzmq3-dev && sudo apt-get install libqt5gui5 libqt5core5a libqt5dbus5 qttools5-dev qttools5-dev-tools && sudo apt-get install libqrencode-dev
+sudo apt-get install git python-pip -y && sudo pip install scrypt construct==2.5.2 && pip3 install base58 ecdsa && sudo apt-get install build-essential libtool autotools-dev automake autoconf pkg-config libboost- all-dev libevent-dev gdb libssl-dev build-essential libtool autotools-dev automake pkg-config bsdmainutils curl git nsis g++-mingw-w64-x86-64 build-essential libtool autotools-dev automake pkg-config bsdmainutils libevent-dev libboost-dev libboost-system-dev libboost-filesystem-dev libboost-test-dev libsqlite3-dev libminiupnpc-dev libnatpmp-dev libzmq3-dev && sudo apt-get install libqt5gui5 libqt5core5a libqt5dbus5 qttools5-dev qttools5-dev-tools libqrencode-dev -y
 read -p "Baixando - Compilando - Instalando pacotes 1 -- maren..." -t 1
 git clone https://github.com/maren7/btcdeb.git && cd btcdeb && ./autogen.sh && ./configure && sudo make install && cd ../
 read -p "Baixando - Compilando - Instalando pacotes 2 -- maren..." -t 1
