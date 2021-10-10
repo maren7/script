cfba0e8bf137169abffb17c02a55bd056b9924d6
Maren <31197527+maren7@users.noreply.github.com>
2021-10-10 15:33:07 -0300

    Update maren-script.sh

--- maren-script.sh
+++ maren-script.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 echo CTRL+C para parar
 read -p "Instalando pacotes -- maren..." -t 1
-sudo apt-get install git python-pip -y && sudo pip install scrypt construct==2.5.2 && pip3 install base58 ecdsa && sudo apt-get install build-essential libtool autotools-dev automake autoconf pkg-config libboost- all-dev libevent-dev gdb libssl-dev build-essential libtool autotools-dev automake pkg-config bsdmainutils curl git nsis g++-mingw-w64-x86-64 build-essential libtool autotools-dev automake pkg-config bsdmainutils libevent-dev libboost-dev libboost-system-dev libboost-filesystem-dev libboost-test-dev libsqlite3-dev libminiupnpc-dev libnatpmp-dev libzmq3-dev && sudo apt-get install libqt5gui5 libqt5core5a libqt5dbus5 qttools5-dev qttools5-dev-tools libqrencode-dev -y
+sudo apt-get install build-essential libtool autotools-dev automake autoconf pkg-config libboost-all-dev libevent-dev gdb libssl-dev build-essential libtool autotools-dev automake pkg-config bsdmainutils curl git nsis g++-mingw-w64-x86-64 build-essential libtool autotools-dev automake pkg-config bsdmainutils libevent-dev libboost-dev libboost-system-dev libboost-filesystem-dev libboost-test-dev libsqlite3-dev libminiupnpc-dev libnatpmp-dev libzmq3-dev -y && sudo apt-get install libqt5gui5 libqt5core5a libqt5dbus5 qttools5-dev qttools5-dev-tools libqrencode-dev -y && sudo apt-get install git python-pip -y && sudo pip install scrypt construct==2.5.2 && pip3 install base58 ecdsa
 read -p "Baixando - Compilando - Instalando pacotes 1 -- maren..." -t 1
 git clone https://github.com/kallewoof/btcdeb.git && ./autogen.sh && ./configure && sudo make install && cd ../
 read -p "Baixando - Compilando - Instalando pacotes 2 -- maren..." -t 1
